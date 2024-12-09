09c340ae7ff3c15d3d537e65555d28d8d25ca9bb
Hammad Ahmad <hamadqur447@gmail.com>
2024-12-09 10:56:10 +0500

    server

--- backend/Server.js
+++ backend/Server.js
@@ -1,6 +1,9 @@
+import dotenv from "dotenv";
+dotenv.config();
 import http, { createServer } from "http";
 import app from "./index.js";
 
 const Server = createServer(app);
+const Port = process.env.PORT;
 
-Server.listen(8080, () => console.log(`Server is running at port  ${8080}`));
+Server.listen(Port, () => console.log(`Server is running at port  ${Port}`));
